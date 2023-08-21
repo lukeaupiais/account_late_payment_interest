93c1d243231f64b37c19921e3f824a81673122cd
Luke Aupiais <luke.aupiais@gmail.com>
2023-08-21 11:34:29 -0300

    Basic Manifest File

---
+++ manifest.py
@@ -0,0 +1,15 @@
+
+{
+    'name': 'Late Payment Interest',
+    'version': '1.0',
+    'summary': 'Add interest on late payments manually',
+    'category': 'Accounting',
+    'author': 'Your Name',
+    'website': 'http://yourwebsite.com',
+    'depends': ['account'],
+    'data': [
+        'views/account_move_views.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
